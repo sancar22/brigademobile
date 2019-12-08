63b6c0d15d5bd293349bcfb3f0ed5bd15da60e5c
sancar22 <santiagoyacaman@uninorte.edu.co>
2019-12-08 00:13:42 -0500

    Added_GPS

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Audio } from "expo-av";
 import {
-    Text,
-    View,
-    StyleSheet,
-    Button,
-    Vibration,
-    Alert,
-    TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Vibration,
+  Alert,
+  TouchableOpacity
 } from "react-native";
 import { Actions } from "react-native-router-flux";
 import * as Permissions from "expo-permissions";
@@ -15,187 +15,208 @@ import * as firebase from "firebase";
 import { Notifications } from "expo";
 import { useSelector, useDispatch } from "react-redux";
 import {
-    notifshow,
-    fillPlace,
-    fillCode,
-    fillCategory,
-    fillDescription,
-    fillInfo,
+  notifshow,
+  fillPlace,
+  fillCode,
+  fillCategory,
+  fillDescription,
+  fillInfo
 } from "../actions/index";
 import _ from "lodash";
 import fb from "../routes/ConfigFire";
 import NotificationContainer from "./NotificationContainer";
-import BackgroundTimer from "react-native-background-timer";
+import * as Location from "expo-location";
 
 function About() {
-    const infoUser = useSelector(state => state.info);
-    const brigada = useSelector(state => state.brigada); //Variable que controlará la visibilidad de la notificación
-    const caso = useSelector(state => state.case);
-    const [sound, setSound] = useState(null);
-    const dispatch = useDispatch();
-    let currentUser = firebase
-        .auth()
-        .currentUser.email.toString()
-        .split(".")[0];
+  const infoUser = useSelector(state => state.info);
+  const brigada = useSelector(state => state.brigada); //Variable que controlará la visibilidad de la notificación
+  const caso = useSelector(state => state.case);
+  const [sound, setSound] = useState(null);
+  const [location, setLocation] = useState(null);
+  const dispatch = useDispatch();
+  let currentUser = firebase
+    .auth()
+    .currentUser.email.toString()
+    .split(".")[0];
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (!user) {
-            Actions.replace("home");
-        }
-    });
-    if (infoUser.ocupado) {
-        Actions.replace("caso");
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+      Actions.replace("home");
     }
-    useEffect(() => {
-        console.log("Mounted About");
-        initializer();
-        register();
-        this.listener = Notifications.addListener(listen);
-        Audio.setAudioModeAsync({
-            staysActiveInBackground: true,
-            allowsRecordingIOS: false,
-            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-            playsInSilentModeIOS: true,
-            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-            playThroughEarpieceAndroid: false,
-            shouldDuckAndroid: true,
-        });
-        return () => {
-            console.log("Unmounted About");
-            this.listener.remove(); // OJO ACÁ CUANDO HAGAMOS MÚLTIPLES PESTAÑAS
-        };
-    }, []);
+  });
+  if (infoUser.ocupado) {
+    Actions.replace("caso");
+  }
+  useEffect(() => {
+    console.log("Mounted About");
+    getLocationAsync();
+    initializer();
+    register();
+    this.listener = Notifications.addListener(listen);
+    Audio.setAudioModeAsync({
+      staysActiveInBackground: true,
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+      shouldDuckAndroid: true
+    });
+    return () => {
+      console.log("Unmounted About");
+      this.listener.remove(); // OJO ACÁ CUANDO HAGAMOS MÚLTIPLES PESTAÑAS
+    };
+  }, []);
+  const getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      alert("No permissions!");
+    } else {
+      console.log("Permissions granted!");
 
-    function initializer() {
-        // When component mounts, there will be a listener for notif sent
-        console.log("UPDATED14");
-        firebase
+      Location.watchPositionAsync(
+        {
+          enableHighAccuracy: false,
+          timeInterval: 30000,
+          distanceInterval: 0
+        },
+        location => {
+          setLocation(location);
+          fb.updateCoords(
+            location.coords.latitude,
+            location.coords.longitude,
+            currentUser
+          );
+        }
+      );
+    }
+  };
+
+  function initializer() {
+    // When component mounts, there will be a listener for notif sent
+    console.log("UPDATED18");
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+
+      .on("value", snapshot => {
+        const allUserInfo = snapshot.val();
+        dispatch(notifshow(allUserInfo.notif));
+        dispatch(fillInfo(allUserInfo));
+      });
+  }
+
+  async function register() {
+    let currentUID = firebase.auth().currentUser.uid.toString();
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    console.log(status);
+    if (status !== "granted") {
+      alert("You need to enable permissions in settings");
+      return;
+    }
+    let token = await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+    fb.addRemainingData(currentUser, currentUID, token);
+  }
+
+  const listen = async ({ origin, data }) => {
+    if (origin === "received") {
+      await Location.getCurrentPositionAsync();
+      Vibration.vibrate(10000);
+      try {
+        const { sound: soundObject, status } = await Audio.Sound.createAsync(
+          {
+            uri:
+              "https://firebasestorage.googleapis.com/v0/b/brigadaun.appspot.com/o/audios%2Falarm.wav?alt=media&token=a2c80767-bae0-47b8-8dae-3b1a7af590df"
+          },
+          { shouldPlay: true }
+        );
+        setSound(soundObject);
+      } catch (error) {
+        console.log(error);
+      }
+
+      fb.setCustomRejectCause(currentUser);
+      firebase
+        .database()
+        .ref("Users/" + currentUser)
+        .once("value", snapshot => {
+          const userInfo = snapshot.val();
+          const notifs = snapshot.val().receivedNotif + 1; // aumentar notificaciones recibidas
+
+          firebase
+            .database()
+            .ref("Casos/" + currentUser + userInfo.receivedNotif) // Para updatear la variable de Redux de caso
+            .once("value", snapshot => {
+              const caseInfo = snapshot.val();
+              dispatch(fillPlace(caseInfo.lugar));
+              dispatch(fillCode(caseInfo.codigo));
+              dispatch(fillDescription(caseInfo.descripcion));
+              dispatch(fillCategory(caseInfo.categoria));
+            });
+          firebase
             .database()
             .ref("Users/" + currentUser)
-            .on("value", snapshot => {
-                const allUserInfo = snapshot.val();
-                dispatch(notifshow(allUserInfo.notif));
-                dispatch(fillInfo(allUserInfo));
-            });
+            .update({ receivedNotif: notifs });
+          // se updatea +1
+        });
+
+      firebase
+        .database()
+        .ref("Users/" + currentUser)
+        .update({ notif: true });
     }
+  };
 
-    async function register() {
-        let currentUID = firebase.auth().currentUser.uid.toString();
-        const { status } = await Permissions.askAsync(
-            Permissions.NOTIFICATIONS
-        );
-        console.log(status);
-        if (status !== "granted") {
-            alert("You need to enable permissions in settings");
-            return;
-        }
-        let token = await Notifications.getExpoPushTokenAsync();
-        console.log(token);
-        fb.addRemainingData(currentUser, currentUID, token);
-    }
+  async function signOutUser() {
+    fb.setOnlineSelected(currentUser);
+    await firebase.auth().signOut();
+    Actions.replace("home");
+  }
 
-    const listen = async ({ origin, data }) => {
-        console.log(origin, data);
+  const rejectCase = () => {
+    fb.increaseRejected(currentUser);
+    sound.stopAsync();
+    Vibration.cancel();
+    Actions.replace("reject");
+    // Para cuando rechace la notificación se oculte la notificación
+  };
 
-        if (origin === "received") {
-            Vibration.vibrate(10000);
-            try {
-                const {
-                    sound: soundObject,
-                    status,
-                } = await Audio.Sound.createAsync(
-                    {
-                        uri:
-                            "https://firebasestorage.googleapis.com/v0/b/brigadaun.appspot.com/o/audios%2Falarm.wav?alt=media&token=a2c80767-bae0-47b8-8dae-3b1a7af590df",
-                    },
-                    { shouldPlay: true }
-                );
-                setSound(soundObject);
-            } catch (error) {
-                console.log(error);
-            }
+  const acceptCase = () => {
+    fb.handleAcceptCase(currentUser);
+    sound.stopAsync();
+    Vibration.cancel();
+    Actions.replace("caso"); // Si acepta se va a la ventana de casos
+  };
 
-            BackgroundTimer.setInterval(() => {
-                console.log("Hello");
-            }, 5000);
-
-            fb.setCustomRejectCause(currentUser);
-            firebase
-                .database()
-                .ref("Users/" + currentUser)
-                .once("value", snapshot => {
-                    const userInfo = snapshot.val();
-                    const notifs = snapshot.val().receivedNotif + 1; // aumentar notificaciones recibidas
-
-                    firebase
-                        .database()
-                        .ref("Casos/" + currentUser + userInfo.receivedNotif) // Para updatear la variable de Redux de caso
-                        .once("value", snapshot => {
-                            const caseInfo = snapshot.val();
-                            dispatch(fillPlace(caseInfo.lugar));
-                            dispatch(fillCode(caseInfo.codigo));
-                            dispatch(fillDescription(caseInfo.descripcion));
-                            dispatch(fillCategory(caseInfo.categoria));
-                        });
-                    firebase
-                        .database()
-                        .ref("Users/" + currentUser)
-                        .update({ receivedNotif: notifs });
-                    // se updatea +1
-                });
-
-            firebase
-                .database()
-                .ref("Users/" + currentUser)
-                .update({ notif: true });
-        }
-    };
-
-    async function signOutUser() {
-        fb.setOnlineSelected(currentUser);
-        await firebase.auth().signOut();
-        Actions.replace("home");
-    }
-
-    const rejectCase = () => {
-        fb.increaseRejected(currentUser);
-        sound.stopAsync();
-        Vibration.cancel();
-        Actions.replace("reject");
-        // Para cuando rechace la notificación se oculte la notificación
-    };
-
-    const acceptCase = () => {
-        fb.handleAcceptCase(currentUser);
-        sound.stopAsync();
-        Vibration.cancel();
-        Actions.replace("caso"); // Si acepta se va a la ventana de casos
-    };
-
-    return (
-        <View style={{ flex: 1 }}>
-            {brigada ? (
-                <NotificationContainer
-                    codigo={caso.codigo}
-                    lugarEmergencia={caso.lugarEmergencia}
-                    categoria={caso.categoria}
-                    descAdicional={caso.descAdicional}
-                    rejectCase={rejectCase}
-                    acceptCase={acceptCase}
-                />
-            ) : (
-                <View style={{ position: "relative", top: 500 }}>
-                    <Button
-                        full
-                        rounded
-                        success
-                        title="logout"
-                        onPress={() => signOutUser()}
-                    />
-                </View>
-            )}
+  return (
+    <View style={{ flex: 1 }}>
+      {brigada ? (
+        <NotificationContainer
+          codigo={caso.codigo}
+          lugarEmergencia={caso.lugarEmergencia}
+          categoria={caso.categoria}
+          descAdicional={caso.descAdicional}
+          rejectCase={rejectCase}
+          acceptCase={acceptCase}
+        />
+      ) : (
+        <View style={{ position: "relative", top: 500 }}>
+          <Button
+            full
+            rounded
+            success
+            title="logout"
+            onPress={() => signOutUser()}
+          />
         </View>
-    );
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
--- src/routes/ConfigFire.js
+++ src/routes/ConfigFire.js
@@ -5,94 +5,101 @@ import React from "react";
 // Initialize Firebase
 
 class Firebase {
-    constructor() {
-        firebase.initializeApp(config);
-    }
+  constructor() {
+    firebase.initializeApp(config);
+  }
 
-    addRemainingData(currentUser, currentUID, token) {
+  addRemainingData(currentUser, currentUID, token) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .update({
+        Expotoken: token,
+        UID: currentUID,
+        online: true,
+        selected: false
+      });
+  }
+
+  setCustomRejectCause(currentUser) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .once("value", snapshot => {
+        let notif = snapshot.val().receivedNotif;
         firebase
-            .database()
-            .ref("Users/" + currentUser)
-            .update({
-                Expotoken: token,
-                UID: currentUID,
-                online: true,
-                selected: false,
-            });
-    }
+          .database()
+          .ref("Casos/" + currentUser + notif)
+          .update({ causaRechazo: "Tiempo agotado" });
+      });
+  }
 
-    setCustomRejectCause(currentUser) {
+  setOnlineSelected(currentUser) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .update({
+        online: false,
+        selected: false
+      });
+  }
+
+  increaseRejected(currentUser) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .once("value", snapshot => {
+        const newRejected = snapshot.val().rejected + 1;
         firebase
-            .database()
-            .ref("Users/" + currentUser)
-            .once("value", snapshot => {
-                let notif = snapshot.val().receivedNotif;
-                firebase
-                    .database()
-                    .ref("Casos/" + currentUser + notif)
-                    .update({ causaRechazo: "Tiempo agotado" });
-            });
-    }
+          .database()
+          .ref("Users/" + currentUser)
+          .update({ rejected: newRejected, notif: false });
+      });
+  }
 
-    setOnlineSelected(currentUser) {
+  handleAcceptCase(currentUser) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .once("value", snapshot => {
+        const newAccepted = snapshot.val().accepted + 1;
+        let received = snapshot.val().receivedNotif - 1;
+        let tI = Date.now() / 1000;
         firebase
-            .database()
-            .ref("Users/" + currentUser)
-            .update({
-                online: false,
-                selected: false,
-            });
-    }
-
-    increaseRejected(currentUser) {
+          .database()
+          .ref("Casos/" + currentUser + received.toString())
+          .update({ tInicial: tI, causaRechazo: "" });
         firebase
-            .database()
-            .ref("Users/" + currentUser)
-            .once("value", snapshot => {
-                const newRejected = snapshot.val().rejected + 1;
-                firebase
-                    .database()
-                    .ref("Users/" + currentUser)
-                    .update({ rejected: newRejected, notif: false });
-            });
-    }
+          .database()
+          .ref("Users/" + currentUser)
+          .update({
+            accepted: newAccepted,
+            ocupado: true,
+            notif: false
+          });
+      });
+  }
 
-    handleAcceptCase(currentUser) {
+  fillTextArea(currentUser, textArea) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .once("value", snapshot => {
+        const notifs = snapshot.val().receivedNotif - 1; // aumentar notificaciones recibidas
+
         firebase
-            .database()
-            .ref("Users/" + currentUser)
-            .once("value", snapshot => {
-                const newAccepted = snapshot.val().accepted + 1;
-                let received = snapshot.val().receivedNotif - 1;
-                let tI = Date.now() / 1000;
-                firebase
-                    .database()
-                    .ref("Casos/" + currentUser + received.toString())
-                    .update({ tInicial: tI, causaRechazo: "" });
-                firebase
-                    .database()
-                    .ref("Users/" + currentUser)
-                    .update({
-                        accepted: newAccepted,
-                        ocupado: true,
-                        notif: false,
-                    });
-            });
-    }
+          .database()
+          .ref("Casos/" + currentUser + notifs) // Para updatear la variable de Redux de caso
+          .update({ causaRechazo: textArea.trim() });
+      });
+  }
 
-    fillTextArea(currentUser, textArea) {
-        firebase
-            .database()
-            .ref("Users/" + currentUser)
-            .once("value", snapshot => {
-                const notifs = snapshot.val().receivedNotif - 1; // aumentar notificaciones recibidas
-
-                firebase
-                    .database()
-                    .ref("Casos/" + currentUser + notifs) // Para updatear la variable de Redux de caso
-                    .update({ causaRechazo: textArea.trim() });
-            });
-    }
+  updateCoords(lat, long, currentUser) {
+    firebase
+      .database()
+      .ref("Users/" + currentUser)
+      .update({ Latitud: lat, Longitud: long });
+  }
 }
 
 export default new Firebase();
